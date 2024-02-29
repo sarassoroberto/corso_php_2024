664f3644f195101641e4e0ca024720dd53394ed9
Mona Lisa <sarassoroberto@gmail.com>
2024-02-29 12:47:17 +0100

    Confronto tra for e foreach

---
+++ 03 Le Variabili - Array - Associative.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Array Associativi</title>
+</head>
+<body>
+    <?php
+    //            0       1        2        3
+    $frutti = [ "pera", "mela", "arancia","mango" ];
+    
+    // Accedo alle informazioni con un indice
+    // la chiave (key) per accedere alle informazioni è un intero
+    $valore_pera = $frutti[0];
+    $valore_arancia = $frutti[2];
+    
+    echo "il valore dell indice 0 è $valore_pera<br>";
+
+
+    $allievo = [
+        // key  => value
+        'nome' => "Antonina",
+        'cognome' => "Efimova"
+    ];
+
+    $allievo_index = ["Antonina","Efimova"];
+    $nome_index = $allievo_index[0];
+
+    $valore_cognome = $allievo['cognome'];
+    $valore_nome = $allievo["nome"];
+
+    echo "il cognome di $valore_nome è $valore_cognome<br>";
+
+    echo "Array dei frutti ha " . count($frutti) . " elementi <br>";
+ 
+    // 
+    for ($i=0; $i < count($frutti) ; $i++) { 
+        echo 'il valore di $i ' . " è $i e corrisponde a  $frutti[$i] <br>";
+    }
+
+    // Dichiarativo
+    foreach ($frutti as $i => $frutto) {
+        echo 'il valore di $i ' . " è $i e corrisponde a  $frutti[$i] <br>"; 
+    }
+
+
+    ?>
+</body>
+</html>
