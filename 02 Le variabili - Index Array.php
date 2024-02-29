ee816838c838937db5145a3bd9586787f45c03d7
Mona Lisa <sarassoroberto@gmail.com>
2024-02-29 11:58:41 +0100

    Rinominato file

--- 02 Le variabili - Index Array.php
+++ 02 Le variabili - Index Array.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>le variabili - Array</title>
+    <title>le variabili - Index Array </title>
 </head>
 <body>
     <?php 
     $frutti = ["arancia","mela","pera"]; 
     $passatempi = array('tennis',"teatro",'programmare',"calcio");
 
-    // echo $frutti;
+  
     // 1 giro $frutto = "arancia"
     // 2 giro $frutto = "mela"
 
@@ -19,6 +19,24 @@
     }
 
     // elenco dei passatempi 
+    // <ul>
+    //     <li>Coffee</li>
+    //     <li>Tea</li>
+    //     <li>Milk</li>
+    // </ul>
+    
+    $nome = "Marco";
+    $frase = "ciao come ti chiami ? mi chiamo " . $nome;
+
+
+    $html = "<ul>"; // "<ul>"
+    foreach ($passatempi as $passatempo) {
+        $html .= '<li>' . $passatempo . '</li>'; 
+    }
+
+    $html .= "</ul>";
+
+    echo $html;
     ?>
 </body>
 </html>
