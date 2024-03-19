7667429c6bc5721260fbce9152d7a3d33e1884fb
Mona Lisa <sarassoroberto@gmail.com>
2024-03-19 16:12:49 +0100

    getJSON

--- lib/jsonTools.php
+++ lib/jsonTools.php
@@ -6,21 +6,31 @@
  getJson("allievi.json","/cache/allievi.json");
  getJson("https://opentdb.com/api.php?amount=10");
  */
- 
-function getJson(string $source,$cache = false){
+
+function getJson(string $source, $cache = false){
     
+    # CREA CARTELLA 
     if($cache && is_string($cache)){
         $directory = __DIR__.dirname($cache); 
-        echo $directory;
         // die();
         if(!file_exists($directory)){
-            echo "non esiste $directory";
-            // creazione della cartella in maniera ricorsiva
             mkdir($directory,0777,true);
-        }else{
-            echo "la cartella esiste $directory";
         }
     }
+
+    if($cache && file_exists($cache)){
+        $content = file_get_contents($cache);
+    }else{
+        $content = file_get_contents($source);
+        if($cache){
+            file_put_contents($cache,$content);
+        }
+    }
+    // return ($content);
+    return json_decode($content,true);
+
+        // se cache  devo aprire la il file cache 
+        // scaricare i dati  e salvarli
+        // salvare i dati se  la  cache non esiste.
 }
 
-getJson("https://opentdb.com/api.php?amount=10","cache/trivia.json");
---
+++ lib/jsonTools_getJson.spec.php
@@ -0,0 +1,40 @@
+<?php
+require_once "./jsonTools.php";
+require_once __DIR__."/jsonTools.php"; // INUTILE
+// require "./jsonTools.php";
+
+echo "\n-------------------";
+echo "\nCASO con  CACHE";
+
+ $result = getJson("https://opentdb.com/api.php?amount=10","cache/trivia.json");
+
+ if(!file_exists("cache")){
+    // __FILE__ --> C:\xampp\htdocs\corso_php_2024\lib\jsonTools_getJson.spec.php
+    echo "\nla cartella è non stata creata";
+    echo "\n".__FILE__.":".__LINE__;
+ }
+
+ if(!file_exists("cache/trivia.json")) {
+     echo "\nil file non è stato creato";
+     echo "\n".__FILE__.":".__LINE__;
+ }
+// getJson("https://opentdb.com/api.php?amount=10","cache/trivia.json");
+
+ // la mia funzione deve restituire un array
+ if(!is_array($result)) {
+     echo "\nmi aspettavo un array invece ho trovato ";
+     var_dump($result);
+     echo "\n".__FILE__.":".__LINE__;
+ }
+
+ echo "\n-----------------------------";
+ echo "\nCASO SENZA CACHE";
+
+
+ $result = getJson('https://opentdb.com/api.php?amount=10');
+
+ if(!is_array($result)) {
+    echo "\nmi aspettavo un array invece ho trovato ";
+    var_dump($result);
+    echo "\n".__FILE__.":".__LINE__;
+}
