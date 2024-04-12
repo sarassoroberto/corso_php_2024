42940ce4517f9392b7465de2f60b06313379e436
Mona Lisa <sarassoroberto@gmail.com>
2024-04-12 12:35:55 +0200

    FIXED

--- 12-opentrivia-app/autoload.php
+++ 12-opentrivia-app/autoload.php
@@ -4,11 +4,12 @@ use model\User;
 // $call = function(){};
 spl_autoload_register(function($class_name){
     $class_name = __DIR__."/".$class_name.".php";
+    if(file_exists($class_name)){
+        include_once $class_name;
+    }
     // print_r($class_name);
-    require_once $class_name;
  
 });
 
-
-new UserCRUD();
-new User();
+// new UserCRUD();
+// new User();
