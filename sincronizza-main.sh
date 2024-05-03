725e65c5253dd6a78c4eb898dc3864c25034ea85
sarassoroberto <sarassoroberto@gmail.com>
2024-05-03 05:30:01 +0200

    aggiunto script sincro

---
+++ sincronizza-main.sh
@@ -0,0 +1,17 @@
+# Visualizza i repository remoti collegati al tuo repository locale
+git remote
+
+# Aggiunge il repository originale come remoto al tuo repository locale
+git remote add upstream https://github.com/corsidrive/corso_php_2024.git
+
+# Recupera le modifiche dal repository originale
+git fetch upstream
+
+# Passa al branch principale (main)
+git checkout main
+
+# Esegue il merge delle modifiche dal repository originale nel tuo branch principale (main)
+git merge upstream/main
+
+# Pusha le modifiche sul tuo fork su GitHub
+git push origin main
