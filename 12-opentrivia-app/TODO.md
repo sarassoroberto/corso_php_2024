9c4b9a5686e6733aaca6bcec80f7ff6af05466e7
sarassoroberto <sarassoroberto@gmail.com>
2024-05-16 07:49:23 +0200

    inserito menu

---
+++ 12-opentrivia-app/TODO.md
@@ -0,0 +1,2 @@
+- Menu di navigazione
+    - user
--- 12-opentrivia-app/view/header.php
+++ 12-opentrivia-app/view/header.php
@@ -1,5 +1,6 @@
-<?php 
+<?php
 use service\autenticate\AutenticateService;
+
 $is_auth = AutenticateService::isAutenticate();
 $user = AutenticateService::getAuthenticatedUser();
 ?>
@@ -15,13 +16,39 @@ $user = AutenticateService::getAuthenticatedUser();
 </head>
 
 <body>
-    
+
     <header class="p-5 bg-primary text-center text-light">
         <h1 class="h3">Rispondituttogame</h1>
-        <?php if($is_auth) : ?>
-            <span><?= $user->nome." ".$user->cognome ?><span>
-            <span><a class="btn btn-outline-light" href="logout.php">logout<a></span>
-        <?php endif ?>
-            
-            </header>
-            <main class="pt-5">
+        <?php if ($is_auth): ?>
+            <span><?= $user->nome . " " . $user->cognome ?><span>
+                    <span><a class="btn btn-outline-light" href="logout.php">logout<a></span>
+                <?php endif ?>
+
+    </header>
+
+        <?php if ($is_auth): ?>
+
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand" href="#">RG</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="users_index.php">Classifica</a>
+                    </li>
+                    <li class="nav-item">
+                        
+                        <a class="nav-link active" aria-current="page" href="nuova_partita.php">Nuova Partita</a>
+                        </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <?php endif ?>
+
+    <main class="pt-5">
